0bbfbd8f642573c51d2b04f13b5da00f4ff36f63
Penetrati0n <fomenko2002ivan@gmail.com>
2021-07-21 20:59:00 +0300

    Added data validation.

--- SimpleLibraryWithBooks/Models/Book/BookModel.cs
+++ SimpleLibraryWithBooks/Models/Book/BookModel.cs
@@ -1,9 +1,21 @@
-﻿namespace SimpleLibraryWithBooks.Models.Book
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleLibraryWithBooks.Models.Book
 {
     public class BookModel
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
+        [RegularExpression("^[\\w'\\-,.][^0-9_!¡?÷?¿\\/\\+=@#$%ˆ&*(){}|~<>;:[\\]]{2,}$",
+            ErrorMessage = "The Author field contains invalid characters.")]
         public string Author { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
         public string Genre { get; set; }
     }
 }
--- SimpleLibraryWithBooks/Models/PersonBook/PersonBookModel.cs
+++ SimpleLibraryWithBooks/Models/PersonBook/PersonBookModel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using SimpleLibraryWithBooks.Models.Book;
 using SimpleLibraryWithBooks.Models.Person;
+using System.ComponentModel.DataAnnotations;
 
 namespace SimpleLibraryWithBooks.Models.PersonBook
 {
     public class PersonBookModel
     {
+        [Required]
         public PersonModel Person { get; set; }
+        
+        [Required]
         public BookModel Book { get; set; }
+        
+        [Required]
         public DateTimeOffset DateTimeReceipt { get; set; }
     }
 }
